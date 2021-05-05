bb95d00ad943c6af9165b9cd4b777e6124542325
David-127 <71015265+David-127@users.noreply.github.com>
2021-05-05 13:36:00 +0200

    animation

---
+++ app.js
@@ -0,0 +1,12 @@
+let animationtext = document.getElementsByClassName('mainindex')
+
+window.addEventListener("scroll", (event) => {
+    console.log(window.scrollY);
+    if (window.scrollY > 250) {
+        // user scrolled more than 250px, do something ...
+
+    }
+    else{
+    
+    }
+})
