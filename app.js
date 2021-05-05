31bc893f7ade98fac9a480d018062f7fca97360a
David-127 <71015265+David-127@users.noreply.github.com>
2021-05-05 14:54:40 +0200

    fail animation

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ let animationtext = document.getElementsByClassName('mainindex')
 
 window.addEventListener("scroll", (event) => {
     console.log(window.scrollY);
-    if (window.scrollY > 250) {
+    if (window.scrollY > 150) {
         // user scrolled more than 250px, do something ...
-
+      element.addClass('mainindex');
     }
     else{
-    
+     element.removeClass('mainindex');
     }
-})
+    })
