f16d3642aaf4e0e79776229bb582621d7409a7b3
David-127 <71015265+David-127@users.noreply.github.com>
2021-05-12 21:16:53 +0200

    i princip klar , små saker återstår

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 let animationtext = document.querySelector('.mainindex');
 let abouttext = document.querySelector('.articleabout');
+let workTextSlide = document.querySelector('.workText');
 
 window.addEventListener("scroll", (event) => {
     console.log(window.scrollY);
     if (window.scrollY > 250) {
-        // user scrolled more than 150px, do something ...
+        // user scrolled more than 250px, do something ...
      animationtext.style.animation='textslide 3s 1' ;
      animationtext.style.visibility='visible';
     }
@@ -13,9 +14,15 @@ window.addEventListener("scroll", (event) => {
     window.addEventListener("scroll", (event) => {
     console.log(window.scrollY);
     if (window.scrollY > 5) {
-        // user scrolled more than 150px, do something ...
-     abouttext.style.animation='aboutslide 3s 1' ;
+     abouttext.style.animation='aboutslide 2s 1' ;
      abouttext.style.visibility='visible';
     }
     })
-    
+
+    window.addEventListener("scroll", (event) => {
+        console.log(window.scrollY);
+        if (window.scrollY > 5) {
+         workTextSlide.style.animation='workslide 1s 1' ;
+         workTextSlide.style.visibility='visible';
+        }
+        })
